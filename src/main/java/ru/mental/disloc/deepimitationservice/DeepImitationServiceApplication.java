4d080d450756abf38f3c778f5e0534c8a7602626
Anton Bukhanets <antoniduass@gmail.com>
2025-03-18 21:57:09 +0300

    Initial commit

---
+++ src/main/java/ru/mental/disloc/deepimitationservice/DeepImitationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mental.disloc.deepimitationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepImitationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepImitationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mental/disloc/deepimitationservice/DeepImitationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mental.disloc.deepimitationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepImitationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
